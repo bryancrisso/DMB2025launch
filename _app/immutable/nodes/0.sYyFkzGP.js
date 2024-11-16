69c936e6f38a646fd21c5460e2fa2eb247b3132d
Bryan Abi Karam <bryancrisso@outlook.com>
2024-11-16 01:34:40 +0000

    Updated mobile view

---
+++ _app/immutable/nodes/0.sYyFkzGP.js
@@ -0,0 +1,1 @@
+import{c as s,a as p}from"../chunks/disclose-version.BNO2ilWn.js";import{b as f,E as i,j as c,k as m,l as d,h,f as l,p as u,m as _,o as v}from"../chunks/runtime.BGCZj4ts.js";function y(e,a,...n){var r=e,o=m,t;f(()=>{o!==(o=a())&&(t&&(d(t),t=null),t=c(()=>o(r,...n)))},i),h&&(r=l)}function T(e,a){u(a,!0);var n=s(),r=_(n);y(r,()=>a.children),p(e,n),v()}export{T as component};
